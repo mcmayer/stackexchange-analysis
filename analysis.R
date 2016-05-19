889ff9c340bff116316f14dd1d05705ed5e00b10
MM <mmayer@mayeranalytics.com>
2016-05-19 08:01:28 +0200

    update

--- analysis.R
+++ analysis.R
@@ -23,16 +23,30 @@ dev.off()
 png('images/answers-users.png', width=600, height=400)
 plot(log10(stackexchange[,'users']), log10(stackexchange[,'answers']),
      xlab="log(#users)", ylab="log(#answers)", main="#answers vs. #users")
+points(log10(stackexchange[1,'users']), log10(stackexchange[1,'answers']), col='red')
 dev.off()
 
 # number of questions answered vs. questions asked
 png('images/questions-answers.png', width=600, height=400)
 plot(log10(stackexchange[,'questions']), log10(stackexchange[,'answers']),
      xlab='log(#questions)', ylab='log(#answers)', main="#answers vs. #questions")
+points(log10(stackexchange[1,'questions']), log10(stackexchange[1,'answers']), col='red')
 dev.off()
 
 # number of questions answered vs. questions asked
-png('images/answered-questions.png', width=600, height=400)
+png('images/answered-users.png', width=600, height=400)
 plot(log10(stackexchange[,'users']), stackexchange[,'answered'],
      xlab='log(#users)', ylab='% answered', main="% answered vs. #users")
+points(log10(stackexchange[1,'users']), stackexchange[1,'answered'], col='red')
 dev.off()
+
+# age vs. users
+png('images/age-users.png', width=600, height=400)
+a = stackexchange[,'age']
+u =  log10(stackexchange[,'users'])
+plot(a, u, xlab='age/years', ylab='log(#users)', main="age/years vs. #users")
+points(a[1], u[1], col='red')
+smu = supsmu(a[-1], u[-1], bass=1)
+lines(smu, col='blue')
+dev.off()
+
